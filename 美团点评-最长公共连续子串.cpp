52bfd9a84018566f445ac1a49ecfc372bb6cc8ce
Marcus Skywalker <marcush1022@163.com>
2017-09-03 08:40:52 +0800

    Update 美团点评-最长公共连续子串.cpp

--- 美团点评-最长公共连续子串.cpp
+++ 美团点评-最长公共连续子串.cpp
@@ -65,3 +65,26 @@ int main()
     cout<<lcs(str1, str2)<<endl;
     return 0;
 }
+
+//若为子序列
+int lcs(string str1, string str2)
+{
+    int len1=str1.length();
+    int len2=str2.length();
+ 
+    int m[len1+1][len2+1];
+    int count=0;
+    for(int i=0; i<=len1; i++)
+    {
+        for(int j=0; j<=len2; j++)
+        {
+            if(i==0 || j==0)
+                m[i][j]=0;
+            else if(str1[i-1]==str2[j-1])
+                m[i][j]=m[i-1][j-1]+1;
+            else
+                m[i][j]=max(m[i-1][j], m[i][j-1]);
+        }
+    }
+    return m[len1][len2];
+}
